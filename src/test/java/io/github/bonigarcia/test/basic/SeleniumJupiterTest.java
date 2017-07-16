d82ea8a6b2d12e63f61dd65cbd2a980ad7bb068c
Boni Garcia <bgarcia@gsyc.es>
2017-07-16 17:31:55 +0200

    Include simple test example ("Hello World" kinda)

---
+++ src/test/java/io/github/bonigarcia/test/basic/SeleniumJupiterTest.java
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.basic;
+
+// tag::snippet-in-doc[]
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.SeleniumExtension;
+
+@ExtendWith(SeleniumExtension.class)
+public class SeleniumJupiterTest {
+
+    @Test
+    public void testWithChrome(ChromeDriver chrome) {
+        chrome.get("https://bonigarcia.github.io/selenium-jupiter/");
+
+        assertTrue(chrome.getTitle().startsWith("selenium-jupiter"));
+    }
+
+    @Test
+    public void testWithChromeAndFirefix(ChromeDriver chrome,
+            FirefoxDriver firefox) {
+        chrome.get("http://www.seleniumhq.org/");
+        firefox.get("http://junit.org/junit5/");
+
+        assertTrue(chrome.getTitle().startsWith("Selenium"));
+        assertTrue(firefox.getTitle().equals("JUnit 5"));
+    }
+
+}
+// end::snippet-in-doc[]
