8e528c0b19952bd0a4cb726506d428f12ffcf94a
Boni Garcia <bgarcia@gsyc.es>
2017-08-03 00:03:36 +0200

    Tests examples with Appium (disable by default because of Travis)

---
+++ src/test/java/io/github/bonigarcia/test/advance/AppiumJupiterTest.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.advance;
+
+//tag::snippet-in-doc[]
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.AppiumDriver;
+import io.github.bonigarcia.Capability;
+import io.github.bonigarcia.DriverCapabilities;
+import io.github.bonigarcia.SeleniumExtension;
+
+@ExtendWith(SeleniumExtension.class)
+public class AppiumJupiterTest {
+
+    @Disabled("Android emulator not available on Travis CI")
+    @Test
+    void testWithAndroid(
+            @DriverCapabilities(capability = {
+                    @Capability(name = "browserName", value = "chrome"),
+                    @Capability(name = "deviceName", value = "Android") }) AppiumDriver<WebElement> android)
+            throws InterruptedException {
+
+        String context = android.getContext();
+        android.context("NATIVE_APP");
+        android.findElement(By.id("com.android.chrome:id/terms_accept"))
+                .click();
+        android.findElement(By.id("com.android.chrome:id/negative_button"))
+                .click();
+        android.context(context);
+
+        android.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertTrue(android.getTitle().contains("JUnit 5 extension"));
+    }
+
+}
+// end::snippet-in-doc[]
---
+++ src/test/java/io/github/bonigarcia/test/advance/AppiumWithGlobalOptionsJupiterTest.java
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.advance;
+
+//tag::snippet-in-doc[]
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.github.bonigarcia.DriverCapabilities;
+import io.github.bonigarcia.DriverUrl;
+import io.github.bonigarcia.SeleniumExtension;
+
+@ExtendWith(SeleniumExtension.class)
+public class AppiumWithGlobalOptionsJupiterTest {
+
+    @DriverUrl
+    String url = "http://localhost:4723/wd/hub";
+
+    @DriverCapabilities
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    {
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("deviceName", "Android");
+    }
+
+    @Disabled("Android emulator not available on Travis CI")
+    @Test
+    void testWithAndroid(AppiumDriver<WebElement> android) {
+        String context = android.getContext();
+        android.context("NATIVE_APP");
+        android.findElement(By.id("com.android.chrome:id/terms_accept"))
+                .click();
+        android.findElement(By.id("com.android.chrome:id/negative_button"))
+                .click();
+        android.context(context);
+
+        android.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertTrue(android.getTitle().contains("JUnit 5 extension"));
+    }
+
+}
+// end::snippet-in-doc[]
