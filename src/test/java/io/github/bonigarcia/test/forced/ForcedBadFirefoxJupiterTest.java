2138f1af0dc50474139f706793893cf0b2959f97
Boni Garcia <bgarcia@gsyc.es>
2017-12-10 22:02:39 +0100

    Include test with invalid binary path for Firefox

---
+++ src/test/java/io/github/bonigarcia/test/forced/ForcedBadFirefoxJupiterTest.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.forced;
+
+import static io.github.bonigarcia.SeleniumJupiter.BINARY;
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.DriverOptions;
+import io.github.bonigarcia.Option;
+import io.github.bonigarcia.SeleniumExtension;
+
+@ExtendWith(SeleniumExtension.class)
+public class ForcedBadFirefoxJupiterTest {
+
+    @BeforeEach
+    void setup() {
+        setProperty("sel.jup.exception.when.no.driver", "false");
+    }
+
+    @Test
+    void operaTest(@DriverOptions(options = {
+            @Option(name = BINARY, value = "/bad/path/to/firefox") }) FirefoxDriver driver) {
+        assertThat(driver, nullValue());
+    }
+
+    @AfterEach
+    void teardown() {
+        clearProperty("sel.jup.exception.when.no.driver");
+    }
+
+}
