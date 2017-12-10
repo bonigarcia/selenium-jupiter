0bf50345c2fd775a9fce37206818512860d843d1
Boni Garcia <bgarcia@gsyc.es>
2017-12-10 19:15:01 +0100

    Include test to force EdgeDriver in a non-Windows machine

---
+++ src/test/java/io/github/bonigarcia/test/forced/ForcedEdgeJupiterTest.java
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.forced;
+
+import static java.lang.System.setProperty;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.SeleniumExtension;
+
+@ExtendWith(SeleniumExtension.class)
+public class ForcedEdgeJupiterTest {
+
+    static {
+        setProperty("sel.jup.exception.when.no.driver", "false");
+        setProperty("wdm.forceOs", "WIN");
+    }
+
+    @Test
+    void edgeTest(EdgeDriver driver) {
+        assumeFalse(IS_OS_WINDOWS);
+        assertThat(driver, nullValue());
+    }
+
+}
