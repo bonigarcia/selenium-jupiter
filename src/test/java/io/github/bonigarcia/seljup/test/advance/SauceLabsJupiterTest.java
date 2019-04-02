9d492979f18e6c48ecf1c4ad3d2f954496d95050
Boni Garcia <bgarcia@gsyc.es>
2019-04-02 15:34:44 +0200

    Include SauceLabs test example and doc

---
+++ src/test/java/io/github/bonigarcia/seljup/test/advance/SauceLabsJupiterTest.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2019 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.seljup.test.advance;
+
+// tag::snippet-in-doc[]
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Disabled;
+// tag::snippet-in-doc[]
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.seljup.DriverCapabilities;
+import io.github.bonigarcia.seljup.DriverUrl;
+import io.github.bonigarcia.seljup.SeleniumExtension;
+
+@Disabled
+// tag::snippet-in-doc[]
+@ExtendWith(SeleniumExtension.class)
+public class SauceLabsJupiterTest {
+
+    @DriverUrl
+    String url = "https://ondemand.eu-central-1.saucelabs.com/wd/hub";
+
+    @DriverCapabilities
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    {
+        capabilities.setCapability("username", "<my-saucelabs-user>");
+        capabilities.setCapability("accessKey", "<my-saucelabs-key>");
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("platform", "Windows 10");
+        capabilities.setCapability("version", "59.0");
+        capabilities.setCapability("name", "selenium-jupiter-and-saucelabs");
+    }
+
+    @Test
+    void testWithSaucelabs(RemoteWebDriver driver) {
+        driver.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertThat(driver.getTitle(),
+                containsString("JUnit 5 extension for Selenium"));
+    }
+
+}
+// end::snippet-in-doc[]
