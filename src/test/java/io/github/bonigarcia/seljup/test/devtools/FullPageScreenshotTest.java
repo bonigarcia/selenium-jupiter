01a4cef64fc151d730a9d1df666952b9f91ff782
Boni García <boni.garcia@uc3m.es>
2021-10-31 15:00:36 +0100

    Include test using DevTools (full screenshot with Chrome)

---
+++ src/test/java/io/github/bonigarcia/seljup/test/devtools/FullPageScreenshotTest.java
@@ -0,0 +1,70 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.seljup.test.devtools;
+
+//tag::snippet-in-doc[]
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v95.dom.model.Rect;
+import org.openqa.selenium.devtools.v95.page.Page;
+import org.openqa.selenium.devtools.v95.page.Page.GetLayoutMetricsResponse;
+import org.openqa.selenium.devtools.v95.page.model.Viewport;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+
+@ExtendWith(SeleniumJupiter.class)
+class FullPageScreenshotTest {
+
+    @Test
+    void testFullPageScreenshot(ChromeDriver driver, DevTools devTools)
+            throws IOException {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/long-page.html");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfNestedElementsLocatedBy(
+                By.className("container"), By.tagName("p")));
+
+        GetLayoutMetricsResponse metrics = devTools
+                .send(Page.getLayoutMetrics());
+        Rect contentSize = metrics.getContentSize();
+        String screenshotBase64 = devTools
+                .send(Page.captureScreenshot(Optional.empty(), Optional.empty(),
+                        Optional.of(new Viewport(0, 0, contentSize.getWidth(),
+                                contentSize.getHeight(), 1)),
+                        Optional.empty(), Optional.of(true)));
+        Path destination = Paths.get("fullpage-screenshot-chrome.png");
+        Files.write(destination, Base64.getDecoder().decode(screenshotBase64));
+
+        assertThat(destination).exists();
+    }
+}
+//end::snippet-in-doc[]
