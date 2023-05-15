344dbc8d811a2018aa673eaaa827abc17ae2706d
Boni Garcia <boni.garcia@uc3m.es>
2023-05-15 10:28:54 +0200

    Disable Opera in Docker test

--- src/test/java/io/github/bonigarcia/seljup/test/docker/DockerOperaTest.java
+++ src/test/java/io/github/bonigarcia/seljup/test/docker/DockerOperaTest.java
@@ -19,6 +19,7 @@ package io.github.bonigarcia.seljup.test.docker;
 import static io.github.bonigarcia.seljup.BrowserType.OPERA;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
@@ -29,6 +30,7 @@ import io.github.bonigarcia.seljup.SeleniumJupiter;
 @ExtendWith(SeleniumJupiter.class)
 class DockerOperaTest {
 
+    @Disabled
     @Test
     void testOpera(@DockerBrowser(type = OPERA) WebDriver driver) {
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
