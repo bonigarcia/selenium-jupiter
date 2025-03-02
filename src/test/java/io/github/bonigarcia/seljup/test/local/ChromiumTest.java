9c0a27e1ed5b97012e83abbc0d34d2bd9dff2604
Boni Garcia <boni.garcia@uc3m.es>
2025-03-02 10:47:19 +0100

    Enable Chromium test and remove custom options

--- src/test/java/io/github/bonigarcia/seljup/test/local/ChromiumTest.java
+++ src/test/java/io/github/bonigarcia/seljup/test/local/ChromiumTest.java
@@ -18,31 +18,17 @@ package io.github.bonigarcia.seljup.test.local;
 
 import static io.github.bonigarcia.seljup.Browser.CHROMIUM;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.openqa.selenium.net.PortProber.findFreePort;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chromium.ChromiumDriver;
 
 import io.github.bonigarcia.seljup.EnabledIfBrowserAvailable;
-import io.github.bonigarcia.seljup.Options;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 
-@Disabled
 @EnabledIfBrowserAvailable(CHROMIUM)
 @ExtendWith(SeleniumJupiter.class)
 class ChromiumTest {
-
-    @Options
-    ChromeOptions options = new ChromeOptions();
-    {
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-debugging-port=" + findFreePort());
-    }
 
     @Test
     void test(ChromiumDriver driver) {
